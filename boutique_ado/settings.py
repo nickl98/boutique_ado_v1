aee4d6414f0718df775803a344450d98b3e734b8
nickl98 <nicklodde98@gmail.com>
2021-07-05 02:49:27 +0000

    Removed secret key and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVLOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nickl98-boutique-ado.herokuapp.com', 'localhost']
 
